05ccb04c9e0cec436abdf0774f46067ab27ffe7b
Nazmul Hassan <Nazmul.naaz96@gmail.com>
2021-09-13 16:59:53 +0100

    Initial commit

---
+++ components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <H2>FULL FIBRE API</H2>
+    <PostList/>
+  </div>
+</template>
+
+<script>
+import PostList from './PostList.vue'
+export default {
+    name: 'App',
+    components: {
+      PostList
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
